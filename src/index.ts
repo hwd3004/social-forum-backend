669d692dd045378771b651f7a95f41cf481d7781
liufeier <hwd3004@gmail.com>
2023-01-02 04:58:55 +0900

    cross-env

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import dotenv from "dotenv";
-dotenv.config();
+import path from "path";
+
+if (process.env.NODE_ENV === "development") {
+  dotenv.config({ path: path.join(__dirname, "../.env.development.local") });
+} else if (process.env.NODE_ENV === "production") {
+  dotenv.config({ path: path.join(__dirname, "../.env.production.local") });
+} else {
+  throw new Error("MISSING ENV.");
+}
+
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
